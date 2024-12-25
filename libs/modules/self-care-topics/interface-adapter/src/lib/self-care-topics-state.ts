2cc611df66979b41775d14795b811b75415cb55c
zhumeisongsong <zhumei.song@atrae.co.jp>
2024-12-25 14:34:25 +0900

    feat: ✨ initial selfCareTopics  is static data for now

--- libs/modules/self-care-topics/interface-adapter/src/lib/self-care-topics-state.ts
+++ libs/modules/self-care-topics/interface-adapter/src/lib/self-care-topics-state.ts
@@ -1,13 +1,15 @@
 import { SelfCareTopic } from '@self-care-topics/domain';
 import { proxy } from 'valtio';
 
+import { selfCareTopics } from './self-care-topics-data';
+
 export type SelfCareTopicsState = {
   selfCareTopics: SelfCareTopic[];
   isLoading: boolean;
 };
 
 export const selfCareTopicsState = proxy<SelfCareTopicsState>({
-  selfCareTopics: [],
+  selfCareTopics: selfCareTopics,
   isLoading: false,
 });
 
