4a9bdefd1822b21ded7f8a579f64c4adcd85f1fc
zhumeisongsong <zhumei.song@atrae.co.jp>
2024-12-25 21:05:22 +0900

    fix: 🐛 lint error

--- libs/shared/ui/src/ui/toggle-group.tsx
+++ libs/shared/ui/src/ui/toggle-group.tsx
@@ -3,7 +3,7 @@ import * as ToggleGroupPrimitive from "@radix-ui/react-toggle-group"
 import { type VariantProps } from "class-variance-authority"
 
 import { cn } from "../utils"
-import { toggleVariants } from "libs/shared/ui/src/ui/toggle"
+import { toggleVariants } from "./toggle"
 
 const ToggleGroupContext = React.createContext<
   VariantProps<typeof toggleVariants>
