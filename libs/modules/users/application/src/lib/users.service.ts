2b65de645d227f42b9ed640c3421d468960f0ac7
zhumeisongsong <zhumei.song@atrae.co.jp>
2024-12-27 15:05:50 +0900

    refactor: ♻️ toggleUserSelfCareTopicUseCase to setXXX

--- libs/modules/users/application/src/lib/users.service.ts
+++ libs/modules/users/application/src/lib/users.service.ts
@@ -8,21 +8,17 @@ export class UsersService {
   private usersRepository: UsersRepositoryImpl;
 
   constructor() {
-    this.usersRepository = new UsersRepositoryImpl(new LocalStorageRepository());
+    this.usersRepository = new UsersRepositoryImpl(
+      new LocalStorageRepository(),
+    );
   }
 
   async getUserSelfCareTopics(): Promise<SelfCareTopic[]> {
     return await this.usersRepository.getUserSelfCareTopics();
   }
 
-  async toggleUserSelfCareTopic(topic: SelfCareTopic) {
-    const topics = await this.usersRepository.getUserSelfCareTopics();
-
-    if (topics.some((item) => item.id === topic.id)) {
-      await this.usersRepository.deleteUserSelfCareTopic(topic);
-    } else {
-      await this.usersRepository.addUserSelfCareTopic(topic);
-    }
+  async setUserSelfCareTopics(topics: SelfCareTopic[]) {
+    await this.usersRepository.setUserSelfCareTopics(topics);
 
     return 'success';
   }
