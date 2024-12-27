69f88b38f82f511a279df7ce44c0228f0e85ed8c
zhumeisongsong <zhumei.song@atrae.co.jp>
2024-12-27 14:40:42 +0900

    fix: 🐛 toggleUserSelfCareTopicUseCase args

--- libs/modules/users/application/src/lib/toggle-user-self-care-topic.use-case.spec.ts
+++ libs/modules/users/application/src/lib/toggle-user-self-care-topic.use-case.spec.ts
@@ -1,9 +1,24 @@
+import { SelfCareTopic } from '@self-care-topics/domain';
 import { toggleUserSelfCareTopicUseCase } from './toggle-user-self-care-topic.use-case';
+import { UsersService } from './users.service';
+
+vi.mock('./users.service');
 
 describe('toggleUserSelfCareTopicUseCase', () => {
-  it('should toggle user self care topic successfully', async () => {
-    const topicId = '123';
-    const result = await toggleUserSelfCareTopicUseCase(topicId);
+  beforeEach(() => {
+    vi.clearAllMocks();
+  });
+
+  it('should toggle self care topic using service', async () => {
+    const mockTopic: SelfCareTopic = { id: '1', name: 'Exercise' };
+
+    const toggleUserSelfCareTopicSpy = vi
+      .spyOn(UsersService.prototype, 'toggleUserSelfCareTopic')
+      .mockResolvedValue('success');
+
+    const result = await toggleUserSelfCareTopicUseCase(mockTopic);
+
+    expect(toggleUserSelfCareTopicSpy).toHaveBeenCalledWith(mockTopic);
     expect(result).toBe('success');
   });
 });
--- libs/modules/users/application/src/lib/toggle-user-self-care-topic.use-case.ts
+++ libs/modules/users/application/src/lib/toggle-user-self-care-topic.use-case.ts
@@ -1,8 +1,10 @@
+import { SelfCareTopic } from "@self-care-topics/domain";
+
 import { UsersService } from "./users.service";
 
-export const toggleUserSelfCareTopicUseCase = async (topicId: string) => {
+export const toggleUserSelfCareTopicUseCase = async (topic: SelfCareTopic) => {
   const usersService = new UsersService();
-  const result = await usersService.toggleUserSelfCareTopic(topicId);
+  const result = await usersService.toggleUserSelfCareTopic(topic);
   
   return result;
 };
