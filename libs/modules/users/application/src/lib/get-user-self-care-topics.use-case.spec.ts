bab6d3d62f04d669b8750d4a09f9c8eedbab7326
zhumeisongsong <zhumei.song@atrae.co.jp>
2024-12-27 14:41:58 +0900

    refactor: ♻️ rename to getUserSelfCareTopics

--- libs/modules/users/application/src/lib/get-user-self-care-topics.use-case.spec.ts
+++ libs/modules/users/application/src/lib/get-user-self-care-topics.use-case.spec.ts
@@ -1,8 +1,27 @@
+import { SelfCareTopic } from '@self-care-topics/domain';
 import { getUserSelfCareTopicsUseCase } from './get-user-self-care-topics.use-case';
+import { UsersService } from './users.service';
+
+vi.mock('./users.service');
 
 describe('getUserSelfCareTopicsUseCase', () => {
-  it('should return an array of self care topics', async () => {
+  beforeEach(() => {
+    vi.clearAllMocks();
+  });
+
+  it('should return self care topics from service', async () => {
+    const mockTopics: SelfCareTopic[] = [
+      { id: '1', name: 'Exercise' },
+      { id: '2', name: 'Meditation' }
+    ];
+
+    const getUserSelfCareTopicsSpy = vi
+      .spyOn(UsersService.prototype, 'getUserSelfCareTopics')
+      .mockResolvedValue(mockTopics);
+
     const result = await getUserSelfCareTopicsUseCase();
-    expect(Array.isArray(result)).toBeTruthy();
+
+    expect(getUserSelfCareTopicsSpy).toHaveBeenCalled();
+    expect(result).toEqual(mockTopics);
   });
 });
--- libs/modules/users/application/src/lib/get-user-self-care-topics.use-case.ts
+++ libs/modules/users/application/src/lib/get-user-self-care-topics.use-case.ts
@@ -2,7 +2,7 @@ import { UsersService } from './users.service';
 
 export const getUserSelfCareTopicsUseCase = async () => {
   const usersService = new UsersService();
-  const result = await usersService.getUserSelfCareCategories();
+  const result = await usersService.getUserSelfCareTopics();
 
   return result;
 };
