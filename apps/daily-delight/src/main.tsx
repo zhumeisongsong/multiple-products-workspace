ee0c468d1ea4b81402e0e954524d0fb9fd4e81a1
zhumeisongsong <zhumei.song@atrae.co.jp>
2025-01-06 19:02:31 +0900

    refactor: ♻️ separate initializeUsersService from UsersService

--- apps/daily-delight/src/main.tsx
+++ apps/daily-delight/src/main.tsx
@@ -1,6 +1,6 @@
 import { ThemeProvider } from '@shared/ui';
 import { InfrastructureContainer } from '@shared/infrastructure-di';
-import { initializeUsersService } from '@users/application';
+import { UsersServiceFactory } from '@users/application';
 import { StrictMode } from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import * as ReactDOM from 'react-dom/client';
@@ -9,7 +9,7 @@ import App from './app/app';
 
 import { globalsStyles } from '@shared/ui';
 
-initializeUsersService(InfrastructureContainer.getUsersRepository());
+UsersServiceFactory.initialize(InfrastructureContainer.getUsersRepository());
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement,
