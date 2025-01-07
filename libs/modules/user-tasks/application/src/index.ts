7bc90601f7bd48679217f4f539449f6ea8d46769
zhumeisongsong <zhumei.song@atrae.co.jp>
2025-01-07 14:30:39 +0900

    feat: ✨ export UserTasksServiceFactory

--- libs/modules/user-tasks/application/src/index.ts
+++ libs/modules/user-tasks/application/src/index.ts
@@ -1,2 +1,4 @@
+export { UserTasksServiceFactory } from './lib/user-tasks-service-factory';
+
 export { getMonthlyUserTasksUseCase } from './lib/get-monthly-user-tasks.use-case';
 export { updateUserTaskStatusUseCase } from './lib/update-user-task-status.use-case';
