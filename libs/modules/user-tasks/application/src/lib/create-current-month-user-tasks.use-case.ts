10ecc488e3214ebd195fd9326b31080685e0c58f
zhumeisongsong <zhumei.song@atrae.co.jp>
2025-01-08 10:33:25 +0900

    feat: ✨ add createCurrentMonthUserTasksUseCase

---
+++ libs/modules/user-tasks/application/src/lib/create-current-month-user-tasks.use-case.ts
@@ -0,0 +1,18 @@
+import { UserPreferences } from '@users/domain';
+import { UserTask } from '@user-tasks/domain';
+
+import { UserTasksService } from './user-tasks.service';
+
+export const createCurrentMonthUserTasksUseCase = async (
+  userPreferences: UserPreferences,
+  userTasksService: UserTasksService,
+): Promise<UserTask[]> => {
+  const userTasks: UserTask[] =
+    await userTasksService.generatedManyUserTasksBasedOnUserPreferences(
+      userPreferences
+    );
+    
+  await userTasksService.createManyUserTasks(userTasks);
+
+  return userTasks;
+};
