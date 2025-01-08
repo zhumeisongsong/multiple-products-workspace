95b860ae638ac239e7b351ff672b7616d0c36658
zhumeisongsong <zhumei.song@atrae.co.jp>
2025-01-08 13:29:47 +0900

    feat: ✨ createOpenAI in OpenAIClient

--- libs/shared/infrastructure/open-ai/src/lib/open-ai-client.ts
+++ libs/shared/infrastructure/open-ai/src/lib/open-ai-client.ts
@@ -1,11 +1,20 @@
-import { openai } from '@ai-sdk/openai';
 import { AIClient } from '@ai/application';
+import { createOpenAI } from '@ai-sdk/openai';
 import { generateText } from 'ai';
 
 export class OpenAIClient implements AIClient {
+  private openai;
+
+  constructor() {
+    this.openai = createOpenAI({
+      // https://nx.dev/recipes/react/use-environment-variables-in-react
+      apiKey: import.meta.env['VITE_OPENAI_API_KEY'],
+    });
+  }
+
   async generateTasks(prompt: string): Promise<string> {
     const result = await generateText({
-      model: openai('gpt-4o'),
+      model: this.openai('gpt-4o-mini'),
       messages: [{ role: 'user', content: prompt }],
     });
 
