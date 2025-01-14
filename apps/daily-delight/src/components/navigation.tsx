2074e4c764f0e82456a0cfbcba1af327bc655c25
zhumeisongsong <zhumei.song@atrae.co.jp>
2025-01-14 15:37:20 +0900

    feat: ✨ max width from md to lg of screen

--- apps/daily-delight/src/components/navigation.tsx
+++ apps/daily-delight/src/components/navigation.tsx
@@ -5,7 +5,7 @@ import { Link } from 'react-router-dom';
 export const Navigation: React.FC = () => {
   return (
     <header className="h-16 flex items-center justify-between">
-      <div className="max-w-screen-md w-full mx-auto px-4 flex justify-between items-center">
+      <div className="max-w-screen-lg w-full mx-auto px-4 flex justify-between items-center">
         <Link to="/">
           <div className="flex items-center gap-2">
             <Sprout strokeWidth={1.7} />
