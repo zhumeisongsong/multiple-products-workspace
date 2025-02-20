2864f801b728e832ed849879e77de2ca4198d5b5
zhumeisongsong <zhumei.song@atrae.co.jp>
2025-02-20 16:20:07 +0900

    feat: ✨ add APIInfrastructureContainer

---
+++ libs/shared/infrastructure/di/src/lib/api-infrastructure-container.ts
@@ -0,0 +1,14 @@
+import { graphqlClient, UsersRepositoryImpl } from '@shared/infrastructure-api';
+import { UsersRepository } from '@users/domain';
+
+export class APIInfrastructureContainer {
+  private static client: typeof graphqlClient;
+  private static usersRepository: UsersRepository;
+
+  static getUsersRepository() {
+    if (!this.usersRepository) {
+      this.usersRepository = new UsersRepositoryImpl(this.client);
+    }
+    return this.usersRepository;
+  }
+}
